d93cf6aed8dbb3e9a9cbc50a162baedc0337734e
Adam Kemeny <adam@kemeny.me>
2019-06-17 15:19:15 +0200

    selection fields stored in state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
 import React from "react";
 import "./App.css";
-import { logo } from "./constants";
+import { logo, star } from "./constants";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { sortByTitle: "A-Z", sortByRating: null };
+    this.handleChangeField = this.handleChangeField.bind(this);
   }
+
+  handleChangeField(event) {
+    const target = event.target,
+      value = target.value,
+      name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">{logo}</header>
+        <header className="App-header">
+          <div>Home</div>
+          <div>Series</div>
+          <div>Movies</div>
+
+          {logo}
+          <div>{star}</div>
+        </header>
+        <div className="body">
+          <h1>Movies</h1>
+          <div className="subBody">
+            <input
+              className=""
+              type="text"
+              name="searchField"
+              placeholder={"Search"}
+              onChange={this.handleChangeField}
+            />
+            Sort By:
+            <select name="sortByTitle" onChange={this.handleChangeField}>
+              <option value="" disabled selected>
+                Title A-Z
+              </option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+            </select>
+            <select name="sortByRating" onChange={this.handleChangeField}>
+              <option value="" disabled selected>
+                Rating
+              </option>
+              <option value="1">⭐</option>
+              <option value="2">⭐⭐</option>
+              <option value="3">⭐⭐⭐</option>
+              <option value="4">⭐⭐⭐⭐</option>
+              <option value="5">⭐⭐⭐⭐⭐</option>
+            </select>
+          </div>
+        </div>
       </div>
     );
   }
